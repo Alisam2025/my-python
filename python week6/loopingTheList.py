638dcafbbbd2b930cc3b40cd7a6c2fbd72354890
mac air 2020 <macair2020@MacBookAir.attlocal.net>
2025-05-20 19:54:24 -0400

    created a new file

---
+++ python week6/loopingTheList.py
@@ -0,0 +1,5 @@
+list_of_integers = [1, 2, 3, 4, 5]
+#for looping through the list
+for sam in list_of_integers:
+    print(sam)  
+        
---
+++ python week6/magician.py
@@ -0,0 +1,5 @@
+magicians = ['alice', 'david', 'carol', 'bob']
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!")
+    print("I can't wait to see your next trick")
+    print()
