2b7416d3fe18ae3d8d76702a38d9d8e84f557b5d
mac air 2020 <macair2020@MacBookAir.attlocal.net>
2025-05-20 18:37:06 -0400

    added fruit lists

--- python week 5/findingLengthOfTheList.py.py
+++ python week 5/findingLengthOfTheList.py.py
@@ -5,3 +5,7 @@ print("Length of the list:", length)
 list_of_lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 length_of_list_of_lists = len(list_of_lists)
 print("Length of the list of lists:", length_of_list_of_lists)
+
+fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape']
+fruit_length = len(fruits)
+print("Length of the fruits list:", fruit_length)
