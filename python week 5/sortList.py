5f37967c472281ce33956a1004e09a8404667db8
mac air 2020 <macair2020@MacBookAir.attlocal.net>
2025-05-20 18:59:29 -0400

    created sort file

---
+++ python week 5/sortList.py
@@ -0,0 +1,16 @@
+fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape']
+fruits.sort()
+print("Sorted fruits list:", fruits)
+
+clothing_brands = ['Nike', 'Adidas', 'Puma', 'Reebok', 'Under Armour']
+clothing_brands.sort()
+print("Sorted clothing brands list:", clothing_brands)
+
+continents = ["South America", "North America", "Europe", "Asia", "Africa", "Australia", "Antarctica"]
+continents.sort()
+print("Sorted continents list:", continents)
+
+#numberical order
+numbers = [5, 2, 9, 1, 5, 6]
+numbers.sort()
+print("Sorted numbers list:", numbers)
