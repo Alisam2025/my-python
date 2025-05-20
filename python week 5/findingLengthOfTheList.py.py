2abfddad86d535ecf9a0bb21a58b0b1ad0295b2d
mac air 2020 <macair2020@MacBookAir.attlocal.net>
2025-05-20 18:28:42 -0400

    added new file

---
+++ python week 5/findingLengthOfTheList.py.py
@@ -0,0 +1,7 @@
+my_list = [1, 2, 3, 4, 5]
+length = len(my_list)
+print("Length of the list:", length)
+
+list_of_lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+length_of_list_of_lists = len(list_of_lists)
+print("Length of the list of lists:", length_of_list_of_lists)
